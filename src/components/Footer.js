99afa9caafa2c6fa69217ade20976c776ba60183
Vitaly Skadorva <vitaly.skadorva@yahoo.com>
2023-12-18 12:17:19 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <footer className="footer" data-cy="footer">
-            <p>Contact us: info@qaapproved.ca</p>
+            <p>Contact us: info@agencyanalytics.com</p>
         </footer>
     );
 }
